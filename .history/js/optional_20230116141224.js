a4e531b5914afc26ff878d7fde193c0cb8ac905b
runeunhjem <runeunhjem@gmail.com>
2023-01-16 21:26:09 +0100

    update style & js

---
+++ .history/js/optional_20230116141224.js
@@ -0,0 +1,175 @@
+// ****************** OPTIONAL *************************
+// INPUT BUTTONS:
+var tvButton = document.querySelector("#tv");
+var pcButton = document.querySelector("#pc");
+var ps5Button = document.querySelector("#ps5");
+var chromecastButton = document.querySelector("#chromecast");
+// APP BUTTONS:
+var netflixButton = document.querySelector("#netflix");
+var disneyButton = document.querySelector("#disney");
+var viaplayButton = document.querySelector("#viaplay");
+var hboButton = document.querySelector("#hbo");
+var discoveryButton = document.querySelector("#discovery");
+var tv2Button = document.querySelector("#tv2");
+var youtubeButton = document.querySelector("#youtube");
+var spotifyButton = document.querySelector("#spotify");
+// CONTROL BUTTONS:
+var powerButton = document.querySelector("#power");
+var volUpButton = document.querySelector("#volume-up");
+var volDownButton = document.querySelector("#volume-down");
+var hdrButton = document.querySelector("#hdr");
+// TV SCREEN:
+var tv__inside = document.querySelector(".tv__inside");
+var activeLogo = document.querySelector("#active-logo");
+
+
+// ALTERNATE TV POWER ON/OFF
+powerButton.onclick = function alternatePower() {
+  resetButtonBackgroundColor();
+  if (tv.tvIsOn === true) {
+    console.log("The TV is on - Turning it off");
+    tv.tvIsOn = false;
+    console.log(tv.tvIsOn);
+    powerButton.style.backgroundColor = "red";
+    tv__inside.style.backgroundColor = "#000";
+    tv__inside.innerHTML = "";
+  } else {
+    console.log("The TV is off - Turning it on");
+    tv.tvIsOn = true;
+    console.log(tv.tvIsOn);
+    powerButton.style.backgroundColor = "green";
+    tv__inside.style.backgroundColor = "darkgrey";
+    tv__inside.innerHTML = "What do you want to watch?";
+  }
+};
+
+// SET PROPERTIES FOR EACH BUTTON
+tvButton.onclick = function () {
+  resetButtonBackgroundColor();
+  tvButton.style.backgroundColor = "#CDCDCD";
+  tvButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#2991F1";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "NRK 1";
+  activeLogo.innerHTML = '<img src="images/nrk-logo-vector.svg" alt="Aktiv Logo">';
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "TV";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+pcButton.onclick = function () {
+  resetButtonBackgroundColor();
+  pcButton.style.backgroundColor = "#CDCDCD";
+  pcButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#F1D129";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "PC";
+  activeLogo.innerHTML = `<img src="images/laptop_icon.svg" alt="Aktiv Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "PC";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+ps5Button.onclick = function () {
+  resetButtonBackgroundColor();
+  ps5Button.style.backgroundColor = "#CDCDCD";
+  ps5Button.style.color = "#000";
+  tv__inside.style.backgroundColor = "#2D4A84";
+  tv__inside.style.color = "#FFF";
+  tv__inside.innerHTML = "Playstation 5";
+  activeLogo.innerHTML = `<img src="images/ps-white_icon.svg" alt="Aktiv Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "Playstation 5";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+chromecastButton.onclick = function () {
+  resetButtonBackgroundColor();
+  chromecastButton.style.backgroundColor = "#CDCDCD";
+  chromecastButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#FA5640";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "Chromecast";
+  activeLogo.innerHTML = `<img src="images/chromecast_icon.svg" alt="Aktiv Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "Chromecast";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+netflixButton.onclick = function () {
+  resetButtonBackgroundColor();
+  netflixButton.style.backgroundColor = "#CDCDCD";
+  netflixButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#FFD9D9";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "Netflix";
+  activeLogo.innerHTML = `<img src="images/netflix-logo_icon.svg" alt="Aktiv Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "Netflix";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+disneyButton.onclick = function () {
+  resetButtonBackgroundColor();
+  disneyButton.style.backgroundColor = "#CDCDCD";
+  disneyButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#D9E6FF";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "Disney+";
+  activeLogo.innerHTML = `<img src="images/disney+_logo.svg" alt="Aktiv Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "Disney";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+viaplayButton.onclick = function () {
+  resetButtonBackgroundColor();
+  viaplayButton.style.backgroundColor = "#CDCDCD";
+  viaplayButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#FFFCED";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "Viaplay";
+  activeLogo.innerHTML = `<img src="images/viaplay_logo.svg" alt="Aktiv Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "Viaplay";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+hboButton.onclick = function () {
+  resetButtonBackgroundColor();
+  hboButton.style.backgroundColor = "#CDCDCD";
+  hboButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#C788F1";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "HBO Max";
+  activeLogo.innerHTML = `<img src="images/HBO_logo.svg" alt="Aktiv Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "HBO Max";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+discoveryButton.onclick = function () {
+  resetButtonBackgroundColor();
+  discoveryButton.style.backgroundColor = "#CDCDCD";
+  discoveryButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#FAA05F";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "discovery+";
+  activeLogo.innerHTML = `<img src="images/Discovery_plus_ico.svg" alt="Active Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "discovery+";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+tv2Button.onclick = function () {
+  resetButtonBackgroundColor();
+  tv2Button.style.backgroundColor = "#CDCDCD";
+  tv2Button.style.color = "#000";
+  tv__inside.style.backgroundColor = "#FAA05F";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "TV2 Play";
+  activeLogo.innerHTML = `<img src="images/TV2-2021.svg" alt="Active Logo"></div>`;
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "TV2 Play";
+  console.log("After ===>", tv.activeTvSource);
+};
---
+++ .history/js/tv_20230116143918.js
@@ -0,0 +1,113 @@
+// ********** My real life object here will be my TV **********
+
+var tv = {
+  // Using var as that is the one used in the modules
+  // The real life object's properties:
+  brand: "Samsung", // String
+  displayType: "QLED", // String
+  screenSize: 65, // Number
+  resolution: "4K", // String
+  hdrIsOn: true, // Boolean
+  isSmartTv: true, // Boolean
+  tvIsOn: true, // Boolean
+  activeTvSource: "PC", // String
+  activeButton: "#pc",
+  availableInputs: ["TV", "PC", "Playstation 5", "Chromecast"], // Array
+  availableApps: [
+    "Netflix",
+    "Disney",
+    "Viaplay",
+    "HBO Max",
+    "discovery+",
+    "TV2 Play",
+    "YouTube",
+    "Spotify",
+  ], // Array
+  currentVolume: 40, // Number
+
+  // ***** Function to adjust the volume: *****
+  adjustVolume: function (volume) {
+    // Functions within an object are also called methods
+    if (volume >= 0 && volume <= 100) {
+      this.currentVolume = volume; // The this keyword will set currentVolume to whatever the adjustVolume("VALUE") value is when calling this function
+      console.log(`Volumelevel is now set to ${volume}`);
+    } else {
+      console.log("Volume level is out of range and invalid");
+    }
+  }, // End of adjustVolume
+
+  // ***** Function to switch TV on or off
+  power: function () {
+    if (this.tvIsOn === true) {
+      console.log("The TV is on");
+      var tvIsOn = true;
+      console.log(tvIsOn);
+      powerButton.style.backgroundColor = "green";
+    } else {
+      console.log("The TV is off");
+      var tvIsOn = false;
+      console.log(tvIsOn);
+      powerButton.style.backgroundColor = "red";
+    }
+  }, // End of power function
+
+  // ***** Function to change either the active TV input or APP: *****
+  changeSource: function (input) {
+    // Functions within an object are also called methods
+    if (
+      this.availableInputs.includes(input) ||
+      this.availableApps.includes(input)
+    ) {
+      // ****** The this keyword points to property's parent (tv) ***********
+      console.log("[Before]", this.activeTvSource);
+      console.log(`Changing source to ${input}`);
+      tv.activeTvSource = input;
+      console.log("[After]", this.activeTvSource);
+    } else {
+      console.log("This does not exist on this TV");
+    }
+  }, // End of changeInput
+}; // **** End of tv object ****
+
+// Running the functions to check if they work as they should in the console:
+tv.adjustVolume(70); // adjust currentVolume to 70 (REMEMBER that adjustVolume is a property of tv)
+tv.adjustVolume(101); // adjust currentVolume to 101 wich is not within the 0 and 100 required in the if(volume...
+console.log("Doublecheck", tv.currentVolume); // Doublecheck currentVolume
+tv.changeSource("HBO Max"); // Does exist (remember tv. first)
+tv.power(); // Get power state and switch to correct power button color and TV image
+
+// *******************************************
+
+// MAKE ARRAY OF ALL BUTTONS:
+var allButtons = [
+  tvButton,
+  pcButton,
+  ps5Button,
+  chromecastButton,
+  netflixButton,
+  disneyButton,
+  viaplayButton,
+  hboButton,
+  discoveryButton,
+  tv2Button,
+  youtubeButton,
+  spotifyButton,
+  powerButton,
+  volUpButton,
+  volDownButton,
+  hdrButton,
+];
+console.log(allButtons);
+
+// MAKE RESET FUNCTION
+function resetButtonBackgroundColor() {
+  allButtons.forEach((button) => {
+    button.style.backgroundColor = "#222";
+    button.style.color = "#FFF";
+    // tv__inside.style.backgroundColor = "darkgrey";
+    // tv__inside.innerHTML = "What do you want to watch?";
+    // activeLogo.innerHTML = `<img src="images/input_icon.svg" alt="Aktiv Logo"></div>`;
+  });
+  powerButton.style.backgroundColor = "green";
+  hdrButton
+}
