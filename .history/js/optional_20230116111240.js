590d362f56f31f9868fac565260b834061c73f52
runeunhjem <runeunhjem@gmail.com>
2023-01-16 12:46:48 +0100

    update style, index & js

---
+++ .history/js/optional_20230116111240.js
@@ -0,0 +1,114 @@
+// ****************** OPTIONAL *************************
+// INPUT BUTTONS:
+var tvButton = document.querySelector("#tv");
+var pcButton = document.querySelector("#pc");
+var ps5Button = document.querySelector("#ps5");
+var chromecastButton = document.querySelector("#chromecast");
+// APP BUTTONS:
+var netflixButton = document.querySelector("#netflix");
+var disneyButton = document.querySelector("#disney");
+var viaplayButton = document.querySelector("#viaplay");
+var hboButton = document.querySelector("#hbo");
+var discoveryButton = document.querySelector("#discovery");
+var tv2Button = document.querySelector("#tv2");
+var youtubeButton = document.querySelector("#youtube");
+var spotifyButton = document.querySelector("#spotify");
+// CONTROL BUTTONS:
+var powerButton = document.querySelector("#power");
+var volUpButton = document.querySelector("#volume-up");
+var volDownButton = document.querySelector("#volume-down");
+var hdrButton = document.querySelector("#hdr");
+// TV SCREEN:
+var tv__inside = document.querySelector(".tv__inside");
+var activeLogo = document.querySelector(".active-logo");
+
+
+// ALTERNATE TV POWER ON/OFF
+powerButton.onclick = function alternatePower() {
+  if (tv.tvIsOn === true) {
+    console.log("The TV is on - Turning it off");
+    tv.tvIsOn = false;
+    console.log(tv.tvIsOn);
+    powerButton.style.backgroundColor = "red";
+    tv__inside.style.backgroundColor = "#111";
+    tv__inside.innerHTML = "";
+  } else {
+    console.log("The TV is off - Turning it on");
+    tv.tvIsOn = true;
+    console.log(tv.tvIsOn);
+    powerButton.style.backgroundColor = "green";
+    tv__inside.style.backgroundColor = "darkgrey";
+    tv__inside.innerHTML = "What do you want to watch?";
+  }
+};
+
+// // MAKE ARRAY OF ALL BUTTONS:
+// const allButtons = [
+//     tvButton,
+//     pcButton,
+//     ps5Button,
+//     chromecastButton,
+//     netflixButton,
+//     disneyButton,
+//     viaplayButton,
+//     hboButton,
+//     discoveryButton,
+//     tv2Button,
+//     youtubeButton,
+//     spotifyButton,
+//     powerButton,
+//     volUpButton,
+//     volDownButton,
+//     hdrButton
+// ];
+// console.log(allButtons);
+
+// // MAKE FUNCTION TO RESET ALL BUTTON BACKGROUNDS
+
+
+tvButton.onclick = function () {
+  resetButtonBackgroundColor();
+  tvButton.style.backgroundColor = "#CDCDCD";
+  tvButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#2991F1";
+  tv__inside.style.color = "#FFF";
+  tv__inside.innerHTML = "NRK 1";
+  activeLogo.style.backgroundImage = "url(../images/nrk-logo-vector.svg)"
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "TV";
+  console.log("After ===>", tv.activeTvSource);
+};
+
+pcButton.onclick = function () {
+  resetButtonBackgroundColor();
+  pcButton.style.backgroundColor = "#CDCDCD";
+  pcButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#F1D129";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "PC";
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "PC";
+  console.log("After ===>", tv.activeTvSource);
+};
+ps5Button.onclick = function () {
+  resetButtonBackgroundColor();
+  ps5Button.style.backgroundColor = "#CDCDCD";
+  ps5Button.style.color = "#000";
+  tv__inside.style.backgroundColor = "#2D4A84";
+  tv__inside.style.color = "#FFF";
+  tv__inside.innerHTML = "Playstation 5";
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "Playstation 5";
+  console.log("After ===>", tv.activeTvSource);
+};
+chromecastButton.onclick = function () {
+  resetButtonBackgroundColor();
+  chromecastButton.style.backgroundColor = "#CDCDCD";
+  chromecastButton.style.color = "#000";
+  tv__inside.style.backgroundColor = "#FA5640";
+  tv__inside.style.color = "#000";
+  tv__inside.innerHTML = "Chromecast";
+  console.log("Before ===>", tv.activeTvSource);
+  tv.activeTvSource = "Chromecast";
+  console.log("After ===>", tv.activeTvSource);
+};
